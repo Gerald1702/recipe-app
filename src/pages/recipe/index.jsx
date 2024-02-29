b68ca54916de6245cd76b5d9b83e3329ea6e7c51
Gerald1702 <ampadugerald17@gmail.com>
2024-02-29 16:50:34 +0000

    recipe app done

--- src/pages/recipe/index.jsx
+++ src/pages/recipe/index.jsx
@@ -1,10 +1,34 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import useSWR from "swr";
+import loadingIcon from "../../asset/images/bouncing-squares.svg";
+import { Container } from "@mui/material";
 
-export default function Recipe () {
-  const {id}= useParams();
+const getRecipe = (...args) => {
+  console.log(args);
+  // prepare URL
+  const url = new URL(args[0]);
+  url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
+  // fetch and return recipe
+  return fetch(url).then(response => response.json())
+}
+
+export default function Recipe() {
+  const { id } = useParams();
+  const { data, isLoading } = useSWR(`https://api.spoonacular.com/recipes/${id}/information`, getRecipe);
+  // console.log(data, isLoading);
 
 
   return (
-    <h1>Recipe Page: {id}</h1>
-  )
+    <>
+      {isLoading ? <img src={loadingIcon} /> : (
+        <Container>
+          <h1>{data.title}</h1>
+          <div>{data.summary}</div>
+          <div dangerouslySetInnerHTML={{ __html: data.summary }} />
+          <img src={data.image} />
+
+        </Container>
+      )}
+    </>
+  );
 }
